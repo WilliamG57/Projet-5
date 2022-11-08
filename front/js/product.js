9627692c2846be127e15dcad3bcddf30c4199402
William Gillet <gillet.william44@gmail.com>
2022-11-08 14:26:51 +0100

    finalisation du choix de la couleur

--- front/js/product.js
+++ front/js/product.js
@@ -16,17 +16,20 @@ fetch(`http://localhost:3000/api/products/${id}`)
 function displayProduct(product) {
     const div = document.getElementsByClassName("item__img");
     const image = document.createElement("img");
-    div[0].appendChild(image);
-    image.src = product.imageUrl;
+    div[0].appendChild(image)
+    image.src = product.imageUrl
     image.alt = product.altTxt;
-    const h1 = document.getElementById("title");
+    const h1 = document.getElementById("title")
     h1.innerText = product.name;
-    const price = document.getElementById("price");
+    const price = document.getElementById("price")
     price.innerText = product.price;
-    const description = document.getElementById("description");
+    const description = document.getElementById("description")
     description.innerText = product.description;
-    const select = document.getElementById("colors").value
-    select[0].appendChild(colors);
-    const option = document.createElement("option").value
-    option.innerText = product.colors;
+    const select = document.getElementById("colors");
+    for (let color of product.colors) {
+        const option = document.createElement("option")
+        select.appendChild(option)
+        option.innerText = color
+        option.value = color;
+    }
 }
